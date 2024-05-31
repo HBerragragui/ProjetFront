11c1789fc108944aa2ff60e55ca2e948a46104a6
HBerragragui <118052363+HBerragragui@users.noreply.github.com>
2024-05-31 07:06:20 +0200

    Update SignIn.vue

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -51,7 +51,7 @@ export default {
     },
     setup() {
         const auth = useAuth()
-        console.log(process.env.BASE_URL)
+        console.log(import.meta.env.VITE_BASE_URL)
         return { auth }
     }
 }
