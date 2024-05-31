715e249cc479bb38759269cacb9f38064aa7214c
HBerragragui <118052363+HBerragragui@users.noreply.github.com>
2024-05-31 06:58:02 +0200

    Update index.js

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = process.env.BASE_URL
+const BASE_URL = import.meta.env.Vite_BASE_URL
 
 export const getAllEmployees = () => {
     return axios.get(BASE_URL)
